c6093199302136296ccd21ab6ad6054f3981bce8
Alison Veras <alisson_veras@hotmail.com>
2020-05-25 01:55:45 -0300

    01_04-Tipos de Parâmetros

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/projects', (req, res) => {
+  const { title, owner} = req.query;
+  console.log(title);
+  console.log(owner);
+
   return res.json(['Project 1', 'Project 2']);
 });
 app.post('/projects', (req, res) => {
+  const { title, owner} = req.body;
+  console.log(title);
+  console.log(owner);
+  
   return res.json(['Project 1', 'Project 2', 'Project 3']);
 });
 app.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
   return res.json(['Project 4', 'Project 2', 'Project 3']);
 });
 app.delete('/projects/:id', (req, res) => {
   return res.json(['Project 2', 'Project 3']);
 });
+
 app.listen(3333, () => {
   console.log('Back-end started!');
 });
