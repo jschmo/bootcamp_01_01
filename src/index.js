84bf3f1a1ec38732b680a72fa9b1709a0e92972f
Alison Veras <alisson_veras@hotmail.com>
2020-05-25 01:25:24 -0300

    01_03-Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello world'});
+app.get('/projects', (req, res) => {
+  return res.json(['Project 1', 'Project 2']);
 });
-
+app.post('/projects', (req, res) => {
+  return res.json(['Project 1', 'Project 2', 'Project 3']);
+});
+app.put('/projects/:id', (req, res) => {
+  return res.json(['Project 4', 'Project 2', 'Project 3']);
+});
+app.delete('/projects/:id', (req, res) => {
+  return res.json(['Project 2', 'Project 3']);
+});
 app.listen(3333, () => {
   console.log('Back-end started!');
 });
